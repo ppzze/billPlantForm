e1ac5733d160edfefd0e62a8601f27eaaf20edd1
lyp <3171790866@qq.com>
2022-03-30 19:55:43 +0800

    error

---
+++ .history/src/views/work_20220327135558.vue
@@ -0,0 +1,15 @@
+<template>
+<div class = "work">
+    <div class="MainTitle2">
+      <div class="MainTitleImg2">
+        <img src="./img/logo.png" alt="">
+      </div>
+      <div class="MainTitleName2">
+        产&nbsp;线&nbsp;管&nbsp;理&nbsp;系&nbsp;统
+      </div>
+    </div>
+</div>   
+</template>
+<script></script>
+<style scoped>
+</style>
---
+++ .history/src/views/work_20220327141718.vue
@@ -0,0 +1,64 @@
+<template>
+<div class = "work">
+    <div class="WorkTitle2">
+      <div class="WorkTitleImg2">
+        <img src="./img/logo.png" alt="">
+      </div>
+      <div class="WorkTitleName2">
+        产&nbsp;线&nbsp;管&nbsp;理&nbsp;系&nbsp;统
+      </div>
+    </div>
+    <div class="body">
+        <div class="video">
+        </div>
+        <div class="process">
+        </div> 
+    </div> 
+</div>   
+</template>
+<script>
+
+</script>
+<style scoped>
+.work{
+  height: 100vh;
+}
+.work .WorkTitle2 {
+  margin: 0 auto;
+  width: 96%;
+  height: 80px;
+  border-bottom: 2px solid #767676;
+}
+.work .WorkTitle2 .WorkTitleImg2{
+  width: 300px;
+  height: 50px;
+  float: left;
+  margin-bottom: 10px;
+  margin-top: 20px;
+  /* position: absolute; */
+  top: 20px;
+  /* bottom: 10px; */
+  padding: 0 50px;
+  border-right: 2px solid #767676;
+}
+.work .WorkTitle2 .WorkTitleImg2 img{
+  width: 100%;
+  height: 100%;
+}
+.work .WorkTitle2 .WorkTitleName2{
+  float: left;
+  height: 80px;
+  margin-left: 50px;
+  top: 20px;
+  padding: 30px 0 0 0;
+  color: white;
+  font-size: 23px;
+  border-bottom: 3px solid;
+  border-image: linear-gradient(to right, #3356bb, #6caacc) 1;
+}
+.work .body{
+  background:red;
+  width: 96%;
+  height: 80px;
+}
+</style>
