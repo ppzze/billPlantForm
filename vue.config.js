1841ed8cad2e7531df650012f16e040bb1cf5101
xingfude <1337473790@qq.com>
2022-04-06 17:42:31 +0800

    1

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,10 @@
 module.exports = {
+    publicPath: './',
+    assetsDir: './supervisor',
+    productionSourceMap: false,
     configureWebpack: {},
     devServer: { // 环境配置
-      // host: '0.0.0.0',
-      // public: '202.206.212.115', // 此处是自己电脑IP地址！
-      // port: '8080',
-      // https: false,
-      // disableHostCheck: true,
-      // open: false // 配置自动启动浏览器
+
         proxy: {
             '/api': {
                 target: 'https://www.chargestation.top',//目标接口域名
